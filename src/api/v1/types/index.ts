8e84f42a14de5bcad48b6affb79113bc7368e8d2
Ahmed-Elzowawi <a.elzowawi@protonmail.com>
2024-09-22 04:36:37 +0100

    Add vehicle type

---
+++ src/api/v1/types/index.ts
@@ -0,0 +1,1 @@
+export * from './vehiclesType.js';
---
+++ src/api/v1/types/vehiclesType.ts
@@ -0,0 +1,11 @@
+export type VehicleType = {
+  manufacturer: string;
+  model: string;
+  fuel: string;
+  type: string;
+  color: string;
+  VIN: string;
+  VRM: string;
+  used: boolean;
+  modelYear: number;
+};
