d93e8cd6667d590432a154291d3c0118eec0a8be
Ahmed-Elzowawi <a.elzowawi@protonmail.com>
2024-09-22 04:06:56 +0100

    Add server routes

---
+++ src/api/v1/routes/routes.ts
@@ -0,0 +1,36 @@
+import express, {
+  ErrorRequestHandler,
+  Express,
+  NextFunction,
+  Request,
+  Response,
+  Router,
+} from 'express';
+import { handleRouters } from '../routers/vehicle_routers';
+
+const entrypoint: string = '/api/v1';
+export const UnavailableRoute = (req: Request, res: Response) => {
+  return res.status(404).end();
+};
+
+const errorHandler: ErrorRequestHandler = (
+  err,
+  req: Request,
+  res: Response,
+  /* eslint-disable  @typescript-eslint/no-unused-vars */
+  next: NextFunction,
+) => {
+  return res.status(400).send({ error: 'Bad Request' });
+};
+
+export const vehiclesRouter: Router = express.Router();
+
+export const handleRoutes = (app: Express): void => {
+  // vehicles
+  app.use(entrypoint, handleRouters(vehiclesRouter));
+
+  // Unavailable endpoints
+  app.use(UnavailableRoute);
+
+  app.use(errorHandler);
+};
