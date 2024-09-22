f89140cd560a71d40824f9e1f4c3b55b32db5186
Ahmed-Elzowawi <a.elzowawi@protonmail.com>
2024-09-22 03:57:49 +0100

    Add methodOverride middleware

--- src/api/v1/middlewares/app_middleware.ts
+++ src/api/v1/middlewares/app_middleware.ts
@@ -1,6 +1,10 @@
+import methodOverride from 'method-override';
 import express, { Express } from 'express';
 
 export const handleAppMiddleware = (app: Express) => {
   // parses incoming requests with JSON payloads
   app.use(express.json());
+
+  // ability to use HTTP verbs PUT or DELETE (client only supports HTTP verbs GET and POST)
+  app.use(methodOverride('_method'));
 };
