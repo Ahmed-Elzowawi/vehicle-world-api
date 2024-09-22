a68c48235853119d4934721facc759f76336ae41
Ahmed-Elzowawi <a.elzowawi@protonmail.com>
2024-09-22 04:28:41 +0100

    Handle app routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Express } from 'express';
+import { handleRoutes } from 'api/v1/routes/index';
 import { handleAppMiddleware } from 'api/v1/middlewares/index';
 
 export const app: Express = express();
 
 handleAppMiddleware(app);
+
+handleRoutes(app);
