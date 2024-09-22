c2f3c32c74eb73fd3524f5f58ca42491e3ff693d
Ahmed-Elzowawi <a.elzowawi@protonmail.com>
2024-09-22 04:20:22 +0100

    Add mongoose credentials types

--- src/api/v1/types/envirement_variables.d.ts
+++ src/api/v1/types/envirement_variables.d.ts
@@ -2,5 +2,10 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
     SERVER_PORT: string;
+    DATABASE_HOST_NAME: string;
+    DATABASE_PORT: string;
+    DATABASE_NAME: string;
+    DATABASE_USER: string;
+    DATABASE_PASSWORD: string;
   }
 }
