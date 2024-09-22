6b242ff2aa12035d9550f90d5bcbf0d968fad607
Ahmed-Elzowawi <a.elzowawi@protonmail.com>
2024-09-22 06:00:24 +0100

    Add vehicle_routers unit test

---
+++ src/api/v1/routers/vehicle_routers.test.ts
@@ -0,0 +1,70 @@
+import {
+  handleRouters,
+  validateVehiclePatchRequestBody,
+  validateVehiclePostRequestBody,
+} from './vehicle_routers';
+import { Router } from 'express';
+import {
+  validateRequestBodyIsEmpty,
+  validateRequestBodyIsJson,
+  validateRequestBodyIsNotEmpty,
+} from 'api/v1/middlewares';
+import {
+  getVehicleController,
+  patchVehicleController,
+  postVehicleController,
+  deleteVehicleController,
+} from 'api/v1/controllers';
+
+describe('Vehcile Routers', () => {
+  const mockedVehicleRouter = {
+    get: jest.fn(),
+    post: jest.fn(),
+    patch: jest.fn(),
+    delete: jest.fn(),
+  } as unknown as Router;
+
+  it('should handle get router', () => {
+    handleRouters(mockedVehicleRouter);
+    expect(mockedVehicleRouter.get).toHaveBeenCalled();
+    expect(mockedVehicleRouter.get).toHaveBeenCalledWith(
+      '/vehicles/:id',
+      validateRequestBodyIsEmpty,
+      getVehicleController,
+    );
+  });
+
+  it('should handle post router', () => {
+    handleRouters(mockedVehicleRouter);
+    expect(mockedVehicleRouter.post).toHaveBeenCalled();
+    expect(mockedVehicleRouter.post).toHaveBeenCalledWith(
+      '/vehicles',
+      validateRequestBodyIsJson,
+      validateRequestBodyIsNotEmpty,
+      validateVehiclePostRequestBody,
+      postVehicleController,
+    );
+  });
+
+  it('should handle patch router', () => {
+    handleRouters(mockedVehicleRouter);
+    expect(mockedVehicleRouter.patch).toHaveBeenCalled();
+    expect(mockedVehicleRouter.patch).toHaveBeenCalledWith(
+      '/vehicles/:id',
+      validateRequestBodyIsJson,
+      validateRequestBodyIsNotEmpty,
+      validateVehiclePatchRequestBody,
+      patchVehicleController,
+    );
+  });
+
+  it('should handle delete router', () => {
+    handleRouters(mockedVehicleRouter);
+    expect(mockedVehicleRouter.delete).toHaveBeenCalled();
+    expect(mockedVehicleRouter.delete).toHaveBeenCalledWith(
+      '/vehicles/:id',
+      validateRequestBodyIsEmpty,
+      deleteVehicleController,
+    );
+  });
+});
