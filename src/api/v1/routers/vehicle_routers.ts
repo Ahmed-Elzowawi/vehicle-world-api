c7476f3402e1b9fa7b4f28fe39b680ba50c6609c
Ahmed-Elzowawi <a.elzowawi@protonmail.com>
2024-09-22 04:50:20 +0100

    Map POST controller to POST router

--- src/api/v1/routers/vehicle_routers.ts
+++ src/api/v1/routers/vehicle_routers.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import {
   getVehicleController,
+  postVehicleController,
 } from 'api/v1/controllers';
 
 
 export const handleRouters = (vehiclesRouter: Router): Router => {
   vehiclesRouter.get('/vehicles/:id', getVehicleController);
 
-  vehiclesRouter.post('/vehicles');
+  vehiclesRouter.post(
+    '/vehicles',
+    postVehicleController,
+  );
 
   vehiclesRouter.patch('/vehicles/:id');
 
